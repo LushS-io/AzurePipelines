9de46f78a2b230a4c6625be30e539b1c46be61a4
Troy <troykirin@gmail.com>
2020-03-07 08:37:53 -0800

    Create, EchoBot object & on_turn func.

---
+++ bot.py
@@ -0,0 +1,32 @@
+# bot.py
+
+# Library Imports
+from sys import exit
+
+
+class EchoBot:
+    # will have properties such as conversation_id, user name, bot name,
+    #  session_url,
+    # basically a bunch of handlers
+
+    async def on_turn(self, context):
+        # we want to take in the activity context and then do something with it
+        # essentially in this case, "message handling?"
+
+        # Check to see if this activity is an incoming message.
+        # (It could theoretically be another type of activity.)
+        if context.activity.type == "message" and context.activity.text:
+            # check if quit statement was said
+            if context.activity.text.lower() == "quit":
+                # say adios
+                await context.send_activity("Thanks for your time, bye!")
+            else:
+                # reply with what was sent
+                await context.send_activity(
+                    f"You said, '{context.activity.text}'"
+                )
+
+            # @GOAL
+            # send the activity back out from bot to repeat the message
+        pass
+    pass
