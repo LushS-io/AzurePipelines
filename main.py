36d6b3fcb9f47d3b8213614d5a850b680f32d959
Troy <troykirin@gmail.com>
2020-03-07 20:24:11 -0800

    comments

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 # main.py
 
-import asyncio
+import asyncio  # async library
 
-from bot import EchoBot
+from bot import EchoBot  # import EchoBot Object from bot
 from adapter import ConsoleAdapter  # how is this finding the adapter "from"
 
 # instanciate the objects
-BOT = EchoBot()
+BOT = EchoBot()  # BOT object calling EchoBot constructor
+# ADAPTER object = the return val of ConsoleAdapter constructor
 ADAPTER = ConsoleAdapter()
 
-# create a looper
+# create an event loop
 LOOP = asyncio.get_event_loop()
 
 
 if __name__ == "__main__":
-    try:
+    try:  # try this except...
         # Greetings
         print("Hi I'm EchoBot, I can repeat what you say to me! Try ME :)")
 
         # Run the loop - unleesh the lava
         LOOP.run_until_complete(ADAPTER.proccess_activity(BOT.on_turn()))
-    except KeyboardInterrupt:
+    except KeyboardInterrupt:  # if a keyboard interrupt happens
         pass
-    finally:
-        LOOP.stop()
-        LOOP.close()
+    finally:  # to finish off the try loop...run these to do a closedown
+        LOOP.stop()  # stop event loop
+        LOOP.close()  # close the loop
