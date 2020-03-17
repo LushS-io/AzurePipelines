5d2a90d0c216476e002528fd6be6b6e150dbb697
Troy <troykirin@gmail.com>
2020-03-16 18:42:04 -0700

    finish

--- adapter/console_adaptor.py
+++ adapter/console_adaptor.py
@@ -1,3 +1,5 @@
+# Troy Kirin edition
+
 import datetime
 import asyncio
 import warnings
@@ -6,7 +8,7 @@ from typing import List, Callable
 from botbuilder.schema import (
     Activity,  # the activity of a bot
     ActivityTypes,  # types of activites bot can handle, such as text, audio, image
-    ChannelAccount,  # the ...
+    ChannelAccount,  # the channel type working with "facebook messenger"
     ConversationAccount,  # conversation ID?
     ResourceResponse,  # hold context?
     ConversationReference,
@@ -18,6 +20,8 @@ from botbuilder.core import (
 from botbuilder.core.bot_adapter import (BotAdapter)
 
 
+# Object Oriented Programming
+# Object of ConsoleAdapter taking in the BotAdapter Object imported from BotBuilder Core
 class ConsoleAdapter(BotAdapter):
     """
     Adapter to communicate with bot via console window
@@ -43,6 +47,7 @@ class ConsoleAdapter(BotAdapter):
             loop.close()
     """
 
+    # __init__ is a special function that
     def __init__(self, reference: ConversationReference = None):
         super(ConsoleAdapter, self).__init__()  # TODO ; learn what this means
 
@@ -94,7 +99,7 @@ class ConsoleAdapter(BotAdapter):
 
         # check if there is a conversation reference meaning,
         #  not a new conversation?
-        if reference is not isinstance(reference, ConversationReference):
+        if reference is not None and not isinstance(reference, ConversationReference):
             # .then() the conditional is done to check if x is not y...
             # .then()
             warnings.warn(
@@ -139,4 +144,103 @@ class ConsoleAdapter(BotAdapter):
                 reference, "activity_id", None
             )
 
+        # ??
+        self._next_id = 0
+
         pass
+
+    async def process_activity(self, logic: Callable):
+        """
+        Begins listening to console input
+        :param logic:
+        :return:
+        """
+        while True:
+            if msg is None:
+                pass
+            else:
+                self._next_id += 1
+                activity = Activity(
+                    text=msg,
+                    channel_id="console",
+                    from_property=ChannelAccount(id="user", name="User1_Troy"),
+                    recipient=ChannelAccount(id="bot", name="Bot"),
+                    conversation=ConversationAccount(id="Convo1"),
+                    type=ActivityTypes.message,
+                    timestamp=datetime.datetime.now(),
+                    id=str(self._next_id),
+                )
+
+                activity = TurnContext.apply_conversation_reference(
+                    activity,
+                    self.reference,
+                    True
+                )
+                context = TurnContext(self, activity)
+                await self.run_pipeline(context, logic)
+
+    async def send_activities(self, context: TurnContext, activities: List[Activity]) -> List[ResourceResponse]:
+        """
+        Logs a series of activites to the console
+        :param context:
+        :param activites:
+        :return:
+        """
+
+        if context is None:
+            raise TypeError(
+                "ConsoleAdapter.send_activites(): `conexxt` argument cannot be None"
+            )
+        if not isinstance(activities, list):
+            raise TypeError(
+                "ConsoleAdapter.send_activites(): `activites` arguemnt must be a list."
+            )
+
+        if len(activities) == 0:
+            raise ValueError(
+                "ConsoleAdapter.send_activites(): `activites` argument cannot have a length of 0."
+            )
+
+        async def next_activity(i: int):
+            responses = []
+
+            if i < len(activities):
+                responses.append(ResourceResponse())
+                activity = activities[i]
+
+                if activity.type == "delay":
+                    await asyncio.sleep(activity.delay)
+                    await next_activity(i + 1)
+                elif activity.type == ActivityTypes.message:
+                    if (
+                        activity.attachments is not None
+                        and len(activity.attachments) > 0
+                    ):
+
+                        append = (
+                            "(1 attachment)"
+                            if len(activity.attachments) == 1
+                            else f"({len(activity.attachmets)} attachments"
+                        )
+                        print(f"{activity.text} {append}")
+
+                    else:
+                        print(f"[{activity.type}]")
+                        await next_activity(i+1)
+
+                else:
+                    return responses
+
+            await next_activity(0)
+
+        async def delete_activity(
+            self, context: TurnContext, reference: ConversationReference
+        ):
+        """
+        """
+        raise NotImplementedError(
+            "ConsoleAdapter.delete_activity(): not supported.")
+
+    async def update_activity(self, context: TurnContext, activity: Activity):
+        raise NotImplementedError(
+            "ConsoleAdapter.update_activity(): not supported.")
